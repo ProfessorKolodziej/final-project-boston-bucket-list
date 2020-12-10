452291e1f8c73a7dbc0a89d85982813b75657daf
Ruotian Bai <rtbai@bu.edu>
2020-12-09 22:31:35 -0500

    changes-evening

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,17 @@
-// Add your scripts here
+/* source: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown */
+function myFunction() {
+  document.getElementById('myDropdown').classList.toggle('show');
+}
+document.querySelector('#hamburger').addEventListener('click', myFunction);
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    const dropdowns = document.getElementsByClassName('dropdown-content');
+    let i;
+    for (i = 0; i < dropdowns.length; i++) { //eslint-disable-line
+      const openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
